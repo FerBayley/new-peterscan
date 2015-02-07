57550e54475376e58d945ae8dc46f81d646e16c8
FerBayley <fernando.bayley@gmail.com>
2015-02-06 21:24:26 -0300

    Correccion en blog y subida de sitio

---
+++ el-perro-sanador.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Peterscan - El perro sanador</title>
+
+	<?php include("includes/data.php"); ?>
+</head>
+<body>
+
+	<section class="Main">
+		
+		<?php include("includes/menu.php"); ?>
+
+		<section class="Contenedor-adiestramiento">
+			<div class="Logo-interno">
+				<a href="index.php">
+					<img src="img/logo-interno.jpg" alt="PeterScan">
+				</a>
+			</div> <!-- End of Logo-interno -->
+		</section> <!-- End of Contenedor-adiestramiento -->
+
+		<section class="Descripcion-seccion">
+			<p>SECCION: El perro sanador</p>
+		</section> <!-- End of Descripcion-seccion -->
+
+		<section class="Contenedor-contenido">
+			<aside>
+				<img src="img/banners/el-perro-sanador.jpg" alt="El perro sanador">
+			</aside> <!-- End of aside -->
+
+			<div class="Contenido">
+				<article>
+					<h2>El perro sanador</h2>
+
+					<p>
+						El perro es un amigo que siempre est&aacute;, que espera, que se 
+						echa a los pies despacito para no molestar, pero haciendo sentir 
+						su c&aacute;lida presencia. Nos mira como solo miran los perros, 
+						cuando se dan cuenta que estamos enfermos, convalecientes o 
+						tristes.
+					</p>
+					
+					<p>
+						Para las personas que padecen una enfermedad, reciben tratamiento
+						m&eacute;dico o psicológico, durantes semanas, meses o a&ntilde;os, 
+						la compa&ntilde;&iacute;a de un perro hace toda la diferencia. 
+						Tener un perro en estas circunstancias, permite sobrellevar mejor 
+						estos momentos en los que nos sentimos vulnerables. Ellos captan 
+						perfectamente nuestras emociones, esta capacidad les permite 
+						entendernos, acompa&ntilde;arnos y hacernos sentir menos solos en 
+						el padecimiento o la dolencia.
+					</p>
+					
+					<p>
+						La capacidad sanadora de los perros est&aacute; en su vitalidad, en 
+						su amor e incondicionalidad y en la entrega que nos ofrecen 
+						cotidianamente para ayudarnos a recuperar la energ&iacute;a vital y 
+						apreciar los peque&ntilde;os momentos del presente.
+					</p>
+				</article>
+			</div> <!-- End of Contenido -->
+		</section> <!-- End of Contenedor-contenido -->
+
+		<?php include("includes/influencia.php"); ?>
+
+		<?php include("includes/redes.php"); ?>
+
+		<?php include("includes/footer.php"); ?>
+		
+	</section> <!-- End of Main -->
+	
+</body>
+</html>
--- includes/influencia.php
+++ includes/influencia.php
@@ -3,36 +3,43 @@
 		<ul>
 			<a href="adiestramiento-up.php">
 				<li>
-					<img src="img/generadores/influencia5.jpg" width="150" height="150" alt="Adiestramiento UP">
+					<img src="img/generadores/influencia5.jpg" width="130" height="130" alt="Adiestramiento UP">
 					<h3>Adiestramiento UP</h3>
 				</li>
 			</a>
 
 			<a href="el-weimaraner.php">
 				<li>
-					<img src="img/generadores/influencia4.jpg" width="150" height="150" alt="Conociendo al Weimaraner">
+					<img src="img/generadores/influencia4.jpg" width="130" height="130" alt="Conociendo al Weimaraner">
 					<h3>Conociendo al Weimaraner</h3>
 				</li>
 			</a>
 
 			<a href="perros-con-poder.php">
 				<li>
-					<img src="img/generadores/influencia2.jpg" width="150" height="150" alt="Perros con poder">
+					<img src="img/generadores/influencia2.jpg" width="130" height="130" alt="Perros con poder">
 					<h3>Perros con poder</h3>
 				</li>
 			</a>
 
 			<a href="influencia-positiva-de-los-perros.php">
 				<li>
-					<img src="img/generadores/influencia.jpg" width="150" height="150" alt="Influencia positiva de los perros">
+					<img src="img/generadores/influencia.jpg" width="130" height="130" alt="Influencia positiva de los perros">
 					<h3>Influencia positiva de los perros</h3>
 				</li>
 			</a>
 
 			<a href="perros-generadores-de-emociones.php">
 				<li>
-					<img src="img/generadores/influencia3.jpg" width="150" height="150" alt="Perros generadores de emociones">
+					<img src="img/generadores/influencia3.jpg" width="130" height="130" alt="Perros generadores de emociones">
 					<h3>Perros generadores de emociones</h3>
+				</li>
+			</a>
+
+			<a href="el-perro-sanador.php">
+				<li>
+					<img src="img/generadores/influencia6.jpg" width="130" height="130" alt="Perros generadores de emociones">
+					<h3>El perro sanador</h3>
 				</li>
 			</a>
 		</ul>
