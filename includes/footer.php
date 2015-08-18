aae4d5d72f945eb44099305537e9502b74a7e12b
FerBayley <fernando.bayley@gmail.com>
2015-08-17 22:11:31 -0300

    Unico commit

--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-	<p>
-		&copy; Todos los derechos reservados 2015 - Peters Can - Adiestramiento Canino: 
-			<a href="mailto:mongesp@yahoo.com.ar">Contacto v&iacute;a email</a>
-	</p>
-</footer> <!-- End of footer -->
